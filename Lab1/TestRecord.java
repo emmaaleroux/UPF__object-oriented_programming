f92a5b7ec3fecde0f00711372ac020be5d86681c
lauramurillo01 <laura.murillo01@estudiant.upf.edu>
2025-10-13 23:14:37 +0200

    Lab1 - testRecord started

--- Lab1/TestRecord.java
+++ Lab1/TestRecord.java
@@ -3,12 +3,14 @@ package Lab1;
 public class TestRecord {
     
     // ATTRIBUTES
-    private Vector v;
-    private Record r;
+    //private Vector v;
+    //private Record r;
 
     // METHODS
 
     // MAIN
-    
+    public static void main(String[] args) {
+        ///////
+    }
 
 }
