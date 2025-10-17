679421823e820a2389e79b3be990c8571ba24ad2
emmaaleroux <emma.leroux01@estudiant.upf.edu>
2025-10-18 00:05:15 +0200

    LAB 1 - optional exercise + updated report

--- Lab1/Record.java
+++ Lab1/Record.java
@@ -26,4 +26,3 @@ public class Record {
         return input.toString() + " -> " + output; 
     }
 }
-
--- Lab1/Vector.java
+++ Lab1/Vector.java
@@ -1,4 +1,9 @@
 package Lab1;
+
+// Optional assignment: more compact methods of Vector
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.DoubleBinaryOperator;
+
 public class Vector {
     
     // ATTRIBUTE
@@ -27,67 +32,67 @@ public class Vector {
         return elems.clone();
     }
 
-    
-    // METHODS
 
-    public Vector add(Vector v) {
+    // Optional assignment: more compact methods of Vector
+    // We need two helper (private) methods to do unary (1 double --> 1 double) binary (2 double --> 1 double) operations.
+
+    private Vector unary(DoubleUnaryOperator op) {
+        // unary takes a unary operator op as a parameter and returns a Vector
+        // the input is of the form (variable -> result)
+        // ex: a -> Math.sqrt(a) (lambda calculus)
+
+        double[] e = new double[elems.length];
+        // for every element of the vector, we apply op
+        for (int i = 0; i < elems.length; i++) {
+            e[i] = op.applyAsDouble(elems[i]);
+        }
+        return new Vector(e);
+    }
+
+    private Vector binary(Vector v, DoubleBinaryOperator op) {
+        // binary takes a binary operator op (like +) as a parameter and returns a Vector
+        // the input is of the form (vector, (a,b) --> result)
+        // ex: v, (a, b) -> (a + b) (lambda calculus)
+
         // We add an exception in order to avoid dimension issues
         if (v.getDim() != elems.length) {
             throw new IllegalArgumentException("Vector dimensions must match");
         }
         double[] e = new double[elems.length];
+        // for every pair of elements, we apply op
         for (int i = 0; i < elems.length; i++) {
-            e[i] = elems[i] + v.elems[i];
+            e[i] = op.applyAsDouble(elems[i], v.elems[i]);
         }
         return new Vector(e);
+    }
+
+
+    // METHODS
+
+    public Vector add(Vector v) {
+        return binary(v, (a, b) -> a + b);
     }
 
     public Vector subtract(Vector v) {
-        // Exception
-        if (v.getDim() != elems.length) {
-            throw new IllegalArgumentException("Vector dimensions must match");
-        }
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            e[i] = elems[i] - v.elems[i];
-        }
-        return new Vector(e);
+        return binary(v, (a, b) -> a - b);
     }
 
     public Vector multiply(Vector v) {
-        // Exception
-        if (v.getDim() != elems.length) {
-            throw new IllegalArgumentException("Vector dimensions must match");
-        }
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            e[i] = elems[i] * v.elems[i];
-        }
-        return new Vector(e);
+        return binary(v, (a, b) -> a * b);
     }
-
+ 
     public Vector divide(Vector v) {
-        // Exception
-        if (v.getDim() != elems.length) {
-            throw new IllegalArgumentException("Vector dimensions must match");
-        }
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            // Another exception
-            if (v.elems[i] == 0) {
+        return binary(v, (a, b) -> {
+            // We check for exceptions to avoid errors
+            if (b == 0) {
                 throw new IllegalArgumentException("Cannot divide by 0");
             }
-            e[i] = elems[i] / v.elems[i];
-        }
-        return new Vector(e);
+            return a / b;
+        });
     }
 
     public Vector multiply(double scalar) {
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            e[i] = elems[i] * scalar;
-        }
-        return new Vector(e);
+        return unary(a -> a * scalar);
     }
 
     public Vector divide(double scalar) {
@@ -95,19 +100,11 @@ public class Vector {
         if (scalar == 0) {
             throw new IllegalArgumentException("Cannot divide by 0");
         }
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            e[i] = elems[i] / scalar;
-        }
-        return new Vector(e);
+        return unary(a -> a / scalar);
     }
 
     public Vector sqrt() {
-        double[] e = new double[elems.length];
-        for (int i = 0; i < elems.length; i++) {
-            e[i] = Math.sqrt(elems[i]);
-        }
-        return new Vector(e);
+        return unary(a -> Math.sqrt(a));
     }
 
     public double dotProduct(Vector v) {
@@ -134,9 +131,7 @@ public class Vector {
     public String toString() {
         String s = "[";
         for (int i = 0; i < elems.length; i++) {
-            if (i > 0) {
-                s+= ", ";
-            }
+            if (i > 0) { s+= ", "; }
             s += elems[i];
         }
         s += "]";
