97d9ecc3951d5bf2060db9443dab99b482c83609
lauramurillo01 <laura.murillo01@estudiant.upf.edu>
2025-11-04 18:50:49 +0100

    lab 3 - SupervisedLearner

--- Lab3/SupervisedLearner.java
+++ Lab3/SupervisedLearner.java
@@ -1,5 +1,54 @@
 package Lab3;
 
 public class SupervisedLearner {
+    // Composition with Model; aggregation with Dataset and Algorithm
+    // Use/Dependency with vector
+
+    //ATRIBUTTES
+    private Algorithm algorithm; 
+    private Dataset dataset;
+    private Model model;
+
+    //CONSTRUCTOR
+    public SupervisedLearner(Algorithm a, Dataset d){
+        algorithm = a;
+        dataset = d;
+        model = null;
+    }
+
+    //GETTER 
+    public Algorithm getAlgorithm(){
+        return algorithm; 
+    }
+
+    public Dataset getDataset(){ 
+        return dataset; 
+    }
+
+    public Model getModel(){ 
+        return model; 
+    }
+
+
+    //METHODS
+    public void solve(){
+        this.model = algorithm.solve(dataset);
+    }
+
+    public double predict(Vector v) {
+        if (model == null) {
+            System.out.println("Model not learned, call solve() first");
+        }
+        Vector vAug = v.augment(); //create x̄ from x
+        return model.predict(vAug); //f(x) = θ · x̄
+    }
+
+    public String toString() {
+        if (model == null) {
+            return "Untrained model, call solve() first";
+        }
+        return model.toString();
+    }
+    
     
 }
