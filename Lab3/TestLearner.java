ed929dabdd76ed1af63ba02c817902c4b20e0135
lauramurillo01 <laura.murillo01@estudiant.upf.edu>
2025-11-04 19:25:35 +0100

    Update TestLearner.java

--- Lab3/TestLearner.java
+++ Lab3/TestLearner.java
@@ -3,5 +3,10 @@ package Lab3;
 public class TestLearner {
     public static void main(String[] args) {
         
-    }  
+        //Error counter (as in the previous Labs)
+        int errors = 0;
+
+        System.out.println("\nErrors found: " + errors);
+        if (errors == 0) {System.out.println("Everything works! \n");}
+    }
 }
