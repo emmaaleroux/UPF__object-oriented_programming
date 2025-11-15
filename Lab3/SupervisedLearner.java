5ab5fa3dc33cc56c888d064d29c62ef794d07143
emmaaleroux <emma.leroux01@estudiant.upf.edu>
2025-11-15 13:29:03 +0100

    LAB 3 - optional 1st try

--- Lab3/SupervisedLearner.java
+++ Lab3/SupervisedLearner.java
@@ -1,4 +1,7 @@
 package Lab3;
+// Optional assignment: we import EJML
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.linsol.svd.SolvePseudoInverseSvd_DDRM;
 
 public class SupervisedLearner {
 
@@ -47,6 +50,46 @@ public class SupervisedLearner {
             return "Untrained model, call solve() first";
         }
         return model.toString();
+    }
+
+    // Optional assignment: Moore-Penrose inverse
+    public Model MPInverse() {
+        // dimension
+        int n = dataset.getData().size();
+        int dim = dataset.getDim() + 1;
+
+        // build X matrix
+        DMatrixRMaj X = new DMatrixRMaj(n, dim);
+
+        for (int i = 0; i < n; i++) {
+            Record r = dataset.getData().get(i);
+            double[] aug = r.getInput().augment().getElems();
+            for (int j = 0; j < dim; j++) {
+                X.set(i, j, aug[j]);
+            }
+        }
+        // build y vector
+        DMatrixRMaj y = new DMatrixRMaj(n, 1);
+        for (int i = 0; i < n; i++) {
+            y.set(i, 0, dataset.getData().get(i).getOutput());
+        }
+
+        // compute pseudoinverse θ* = X† y
+        DMatrixRMaj theta = new DMatrixRMaj(dim, 1); // result
+        SolvePseudoInverseSvd_DDRM pinv = new SolvePseudoInverseSvd_DDRM();
+        pinv.setA(X);       // compute pseudoinverse of X
+        pinv.solve(y, theta);
+
+        // return model
+        double[] params = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            params[i] = theta.get(i, 0);
+        }
+
+        Model m = new Model(dim);
+        m.update(new Vector(params).multiply(-1), -1);    // force replace
+
+        return m;
     }
     
 }
