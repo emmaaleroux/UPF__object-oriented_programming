8cd046013a8551205fd5d8477347b5680e957a49
lauramurillo01 <laura.murillo01@estudiant.upf.edu>
2025-11-25 16:11:08 +0100

    lab 4 - StdarizedDataset changes

--- Lab4/StandardizedDataset.java
+++ Lab4/StandardizedDataset.java
@@ -37,6 +37,14 @@ public class StandardizedDataset extends Dataset {
     // METHODS
     @Override
     public Record transform(Record r){ 
+        // We check that we are not dividing by zero to avoid errors
+        for (double s : si.getElems())
+            if (s == 0.0) {
+                throw new IllegalStateException("Input std = 0, can't standardize.");
+            }
+        if (so == 0.0) {
+            throw new IllegalStateException("Output std = 0, can't standardize.");
+        }
         // compute a transformed input x̂ = (x − μ_in)/σ_in and reverse v · σ_out + μ_out
         // standardize input
         Vector x = r.getInput(); //get input vector x
@@ -55,35 +63,6 @@ public class StandardizedDataset extends Dataset {
         return new Record(xStd, yStd); //return standarized record
     }
 
-    /*
-    @Override
-    public Record transform(Record r) {
-        // We check that we are not dividing by zero to avoid errors
-        for (double s : si.getElems())
-            if (s == 0.0) {
-                throw new IllegalStateException("Input std = 0, can't standardize.");
-            }
-        if (so == 0.0) {
-            throw new IllegalStateException("Output std = 0, can't standardize.");
-        }
-
-        // We standardize the input (x: Vector) as x' = (x - mi) / si 
-        Vector standardInput = r.getInput().subtract(mi).divide(si);
-        // We round the input to 5 decimals
-        double[] roundedStandard = standardInput.getElems();
-        for (int i = 0; i < roundedStandard.length; i++) {
-            roundedStandard[i] = round5(roundedStandard[i]);
-        }
-        standardInput = new Vector(roundedStandard);
-
-        // We standardize the output (y: double) as y' = (y - mo) / so
-        double standardOutput = (r.getOutput() - mo) / so;
-        //We round the output to 5 decimals
-        standardOutput = round5(standardOutput);
-        
-        return new Record(standardInput, standardOutput);
-    }
-    */
 
     @Override
     public double output(double d){
